068126be6dca21a58e2e70adaa2e5927f559fa99
RicardoRR29 <r3ifes@gmail.com>
2021-04-09 10:06:29 -0300

    set State and using an fetch

--- teste-app/src/App.js
+++ teste-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'; 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,28 +6,22 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstrein',
-          id: 'asc1'
-        },
-        {
-          name: 'Drakula',
-          id: 'asr2'
-        },
-        {
-          name: 'Zombie',
-          id: 'as1w'
-        }
-      ]
+      monsters: []
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response =>response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1>{ monster.name }</h1>)
-        }
+        {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{ monster.name }</h1>
+        ))}
       </div>
     );
   }
